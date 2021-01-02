168a27154bddbcb79b46db159d6f86de7584e939
Darpan Pednekar <pednekardarpan@gmail.com>
2021-01-02 17:44:52 +0530

    https://github.com/darpan45/Angular.git

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverID: number = 12;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    if (this.serverID == 10) {
+      return this.serverStatus;
+    } else {
+      this.serverStatus = 'Online';
+      return this.serverStatus;
+    }
+  }
+}
